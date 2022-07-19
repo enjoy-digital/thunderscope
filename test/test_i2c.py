d3f3fe1e003c367c57aa895e56025e252e95978a
Florent Kermarrec <florent@enjoy-digital.fr>
2022-07-19 18:56:10 +0200

    test/test_i2c: Add MCP4728 test.

--- test/test_i2c.py
+++ test/test_i2c.py
@@ -23,6 +23,15 @@ I2C_DELAY = 1
 I2C_WRITE = 0
 I2C_READ  = 1
 
+def I2C_W_ADDR(addr):
+    return (addr & 0x7f) << 1
+
+def I2C_R_ADDR(addr):
+    return ((addr & 0x7f) << 1) | 1
+
+MCP4728_I2C_ADDR = 0x61
+LMK61E2_I2C_ADDR = 0x58
+
 # BitBangI2C ---------------------------------------------------------------------------------------
 
 class BitBangI2C:
@@ -90,12 +99,12 @@ class BitBangI2C:
 
     def poll(self, addr):
         self.start_cond()
-        ack  = self.write(addr << 1 | 0)
-        ack |= self.write(addr << 1 | 1)
+        ack  = self.write(I2C_W_ADDR(addr))
+        ack |= self.write(I2C_R_ADDR(addr))
         self.stop_cond()
         return ack
 
-# I2C Scan Test ------------------------------------------------------------------------------------
+# I2C Scan -----------------------------------------------------------------------------------------
 
 def i2c_scan_test(host, port):
     bus = RemoteClient(host=host, port=port)
@@ -115,6 +124,52 @@ def i2c_scan_test(host, port):
         sys.stdout.flush()
     print("")
 
+    bus.close()
+
+# MCP4728 Test -------------------------------------------------------------------------------------
+
+def mcp4728_test(host, port):
+    bus = RemoteClient(host=host, port=port)
+    bus.open()
+
+    class MCP4728:
+        def __init__(self, addr):
+            self.addr = addr
+            self.i2c  = BitBangI2C(bus.regs)
+
+        def reset(self):
+            # General Call Reset.
+            self.i2c.start_cond()
+            self.i2c.write(I2C_W_ADDR(MCP4728_I2C_ADDR))
+            self.i2c.write(0x00)
+            self.i2c.write(0x06)
+            self.i2c.stop_cond()
+
+        def set_ch(self, n, value):
+            value &= 0xfff
+            self.i2c.start_cond()
+            self.i2c.write(I2C_W_ADDR(MCP4728_I2C_ADDR))
+            self.i2c.write(0x40 + (n << 1))
+            self.i2c.write((value >> 8) & 0x0f)
+            self.i2c.write((value >> 0) & 0xff)
+            self.i2c.stop_cond()
+
+
+    mcp4728 = MCP4728(addr=0x61)
+    mcp4728.reset()
+
+    print("MCP4728 test...")
+    for i in range(4):
+        print(f"{i}/4")
+        print("Setting channels to 0.")
+        for n in range(4):
+            mcp4728.set_ch(n=n, value=0)
+        print("Setting channels to 2**12-1.")
+        for n in range(4):
+            mcp4728.set_ch(n=n, value=2**12-1)
+
+    bus.close()
+
 # Run ----------------------------------------------------------------------------------------------
 
 def main():
@@ -122,14 +177,18 @@ def main():
     parser.add_argument("--host",    default="localhost",   help="Host ip address")
     parser.add_argument("--port",    default="1234",        help="Host bind port.")
 
-    parser.add_argument("--scan", action="store_true", help="Scan I2C Bus.")
+    parser.add_argument("--scan",         action="store_true", help="Scan I2C Bus.")
+    parser.add_argument("--mcp4728-test", action="store_true", help="Test MCP4728 I2C Access/Config.")
     args = parser.parse_args()
 
     host = args.host
     port = int(args.port, 0)
 
     if args.scan:
-        i2c_scan_test(host=host, port=port)
+        i2c_scan(host=host, port=port)
+
+    if args.mcp4728_test:
+        mcp4728_test(host=host, port=port)
 
 if __name__ == "__main__":
     main()
