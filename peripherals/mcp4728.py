f033797348e256e8da52bac8221bb1f456344cdf
Florent Kermarrec <florent@enjoy-digital.fr>
2022-07-25 15:57:06 +0200

    periphelals: Add mcp4728 driver and switch test_i2c to it.

---
+++ peripherals/mcp4728.py
@@ -0,0 +1,57 @@
+#
+# This file is part of Thunderscope-LiteX project.
+#
+# Copyright (c) 2022 Florent Kermarrec <florent@enjoy-digital.fr>
+# SPDX-License-Identifier: BSD-2-Clause
+
+import sys
+
+from .i2c import *
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+#                               D E S C R I P T I O N                                              #
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+
+# N/A.
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+#                               D E F I N I T I O N S                                              #
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+
+MCP4728_I2C_ADDR = 0x61
+
+MCP4728_GENERAL_CALL_COMMAND = 0x00
+MCP4728_GENERAL_CALL_RESET   = 0x06
+MCP4728_WRITE_DAC_REGISTER   = 0x40
+
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+#                                  G A T E W A R E                                                 #
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+
+# N/A (Directly reusing LiteX's bitbang I2C core).
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+#                                  S O F T W A R E                                                 #
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+
+class MCP4728Driver:
+    def __init__(self, bus, name, addr):
+        self.i2c  = I2CDriver(bus=bus, name=name)
+        self.addr = addr
+
+    def rst(self):
+        self.i2c.start_cond()
+        self.i2c.write(I2C_W_ADDR(self.addr))
+        self.i2c.write(MCP4728_GENERAL_CALL_COMMAND)
+        self.i2c.write(MCP4728_GENERAL_CALL_RESET)
+        self.i2c.stop_cond()
+
+    def set_ch(self, n, value):
+        assert n < 4
+        self.i2c.start_cond()
+        self.i2c.write(I2C_W_ADDR(self.addr))
+        self.i2c.write(MCP4728_WRITE_DAC_REGISTER + (n << 1))
+        self.i2c.write((value >> 8) & 0x0f)
+        self.i2c.write((value >> 0) & 0xff)
+        self.i2c.stop_cond()
--- test/test_i2c.py
+++ test/test_i2c.py
@@ -14,11 +14,7 @@ from litex import RemoteClient
 
 sys.path.append("..")
 from peripherals.i2c import *
-
-# I2C Constants ------------------------------------------------------------------------------------
-
-MCP4728_I2C_ADDR = 0x61
-LMK61E2_I2C_ADDR = 0x58
+from peripherals.mcp4728 import *
 
 # I2C Scan -----------------------------------------------------------------------------------------
 
@@ -48,31 +44,8 @@ def mcp4728_test(host, port):
     bus = RemoteClient(host=host, port=port)
     bus.open()
 
-    class MCP4728:
-        def __init__(self, addr):
-            self.addr = addr
-            self.i2c  = I2CDriver(bus=bus, name="i2c")
-
-        def reset(self):
-            # General Call Reset.
-            self.i2c.start_cond()
-            self.i2c.write(I2C_W_ADDR(self.addr))
-            self.i2c.write(0x00)
-            self.i2c.write(0x06)
-            self.i2c.stop_cond()
-
-        def set_ch(self, n, value):
-            value &= 0xfff
-            self.i2c.start_cond()
-            self.i2c.write(I2C_W_ADDR(self.addr))
-            self.i2c.write(0x40 + (n << 1))
-            self.i2c.write((value >> 8) & 0x0f)
-            self.i2c.write((value >> 0) & 0xff)
-            self.i2c.stop_cond()
-
-
-    mcp4728 = MCP4728(addr=MCP4728_I2C_ADDR)
-    mcp4728.reset()
+    mcp4728 = MCP4728Driver(bus=bus, name="i2c", addr=MCP4728_I2C_ADDR)
+    mcp4728.rst()
 
     print("MCP4728 test...")
     for i in range(4):
